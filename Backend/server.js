340a4142384b748d581fea4c8c207bad4832328b
Sakesh120 <sakeshchopkar927@gmail.com>
2026-04-04 17:00:37 +0530

    DateBase connected successfully

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,7 @@
 import app from "./src/app.js";
+import connectDB from "./src/config/database.js";
+
+connectDB();
 
 app.listen(3000, () => {
   console.log("The server is runnig on  the http://localhost:3000");
