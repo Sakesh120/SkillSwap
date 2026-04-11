238454dd6ff1431125cc59a951b86e4d44b22e43
Sakesh120 <sakeshchopkar927@gmail.com>
2026-04-11 12:57:11 +0530

    get profile and update profile apis created

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,6 +4,7 @@ import authRouter from "./routes/auth.route.js";
 import matchRouter from "./routes/match.routes.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import userRouter from "./routes/user.routes.js";
 
 const app = express();
 
@@ -14,4 +15,6 @@ app.use(cors());
 
 app.use("/api/auth", authRouter);
 app.use("/api/match", matchRouter);
+app.use("/api/users", userRouter);
+
 export default app;
