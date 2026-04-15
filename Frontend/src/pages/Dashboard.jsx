064bfe85691e8e57256aa3401f28e9c3b6d9bda5
RohitKhandalkar <rohitkhandalkar2006@gmail.com>
2026-04-15 15:39:35 +0530

    ...

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -9,9 +9,9 @@ import Activity from "../components/dashboard/Activity";
 function Dashboard() {
   
   return (
-    <div className="min-h-screen bg-linear-to-br from-blue-50 via-white to-indigo-100">
+    <div className="min-h-screen min-w-screen bg-linear-to-br from-blue-50 via-white to-indigo-100">
 
-      <div className="max-w-6xl mx-auto p-4 md:p-6 space-y-8">
+      <div className="max-w-6xl mx-auto p-4 md:p-6 space-y-8 ">
 
         <SuggestedMatches />
         <RequestsOverview />
