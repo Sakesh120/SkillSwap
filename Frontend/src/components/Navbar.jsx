60c46cb7ac5f0509c5079696aa3914588a30063c
RohitKhandalkar <rohitkhandalkar2006@gmail.com>
2026-04-15 19:30:49 +0530

    navbar update

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,11 +1,11 @@
-import { Link } from "react-router-dom";
+import { Link, redirect } from "react-router-dom";
 import logo from '../assets/logo.png';
 import { useState, useEffect } from "react";
 import { useAuth } from "../context/AuthContext";
 
 function Navbar() {
 
-const { user } = useAuth();
+const { user, logout } = useAuth();
 
      const [show, setShow] = useState(true);
   const [lastScrollY, setLastScrollY] = useState(0);
@@ -31,7 +31,7 @@ const { user } = useAuth();
   return (
     <div className="fixed top-0 left-0 right-0 flex justify-between items-center px-8 py-1 bg-blue/30 backdrop-blur-sm z-50  transition-transform duration-300" style={{ transform: show ? "translateY(0)" : "translateY(-100%)"   
      }}>
-      <img src={logo} alt="Logo" className="w-20 h-20 object-contain" />
+      <Link to="/"> <img src={logo} alt="Logo" className="w-20 h-20 object-contain " /> </Link>
 
       <div className="flex items-center space-x-8 text-xl  text-black">
         <Link to="/">Home</Link>
@@ -40,7 +40,20 @@ const { user } = useAuth();
        {user ? (
   <div className="flex flex-row items-center gap-4">
     <span>🔔</span>
-    <div className="w-10 h-10 bg-gray-300 rounded-full"></div>
+  <div
+  className="w-10 h-10 rounded-full overflow-hidden bg-gray-300 cursor-pointer"
+>
+  {user?.image && (
+    <img src={user.image} className="w-full h-full object-cover" />
+  )}
+</div>
+<button
+  onClick={logout}
+  className="bg-red-400 text-white px-3 py-1 rounded"
+>
+  Logout
+</button>
+
   </div>
 ) : (
   <div className="flex gap-4">
