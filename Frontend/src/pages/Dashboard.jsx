9e441b6c7cd871e4f69322614696e273c097ba49
Sakesh120 <sakeshchopkar927@gmail.com>
2026-04-17 09:23:35 +0530

    improve the margin of dashboard

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -7,18 +7,14 @@ import ActiveSessions from "../components/dashboard/ActiveSessions";
 import Activity from "../components/dashboard/Activity";
 
 function Dashboard() {
-  
   return (
-    <div className="min-h-screen min-w-screen bg-linear-to-br from-blue-50 via-white to-indigo-100">
-
-      <div className="max-w-6xl mx-auto p-4 md:p-6 space-y-8 ">
-
+    <div className=" min-h-screen min-w-screen bg-linear-to-br from-blue-50 via-white to-indigo-100">
+      <div className="lg:mt-24 mt-26 max-w-6xl mx-auto p-4 md:p-6 space-y-8 ">
         <SuggestedMatches />
         <RequestsOverview />
         <PromoCards />
         <ActiveSessions />
         <Activity />
-
       </div>
     </div>
   );
